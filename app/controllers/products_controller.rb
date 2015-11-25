6ea99cb9f0479be9e99cb430d19785fb0a7d39c6
Heart Beat Studio <blacklight728@gmail.com>
2015-11-24 19:11:55 -0500

    Modify Product controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,17 @@
 class ProductsController < ApplicationController
   def index
+    if current_cart
+      @cart = current_cart
+    else
+      @cart = Cart.new
+    end
+    session[:cart_id] = cart.id
+    @orders = @cart.orders
+    @order = @cart.orders.new
     @products = Product.all
   end
 
   def show
     @product = Product.find(params[:id])
-  end
-
-  
+  end  
 end
