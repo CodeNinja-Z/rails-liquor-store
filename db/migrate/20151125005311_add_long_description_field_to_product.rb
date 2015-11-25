642383881815a68c23f122203154d0b4b060a9e2
Heart Beat Studio <blacklight728@gmail.com>
2015-11-24 20:00:00 -0500

    Add field to Product table. Add image uploader.

---
+++ db/migrate/20151125005311_add_long_description_field_to_product.rb
@@ -0,0 +1,5 @@
+class AddLongDescriptionFieldToProduct < ActiveRecord::Migration
+  def change
+    add_column :products, :long_description, :text
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.create!({
+  name: "",
+  description: "Bordeaux Merlot",
+  long_description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur at inventore molestiae dolorem esse nostrum similique reiciendis, impedit recusandae voluptas quia quibusdam aperiam quam quidem, temporibus nisi necessitatibus, magni aspernatur.",
+  image: "",
+  category: "red wine"
+  })
+
+Package.create!({
+  
+  })
+
+Product.create!({
+  name: "",
+  description: "Beaune Melbec",
+  long_description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum error voluptates possimus eveniet recusandae blanditiis asperiores, impedit iste eaque ut quas cupiditate ad incidunt fugiat molestiae! Hic id commodi cum!",
+  image: "",
+  category: "red wine"
+  })
